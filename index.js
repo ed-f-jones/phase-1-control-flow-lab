0a551d97391a425b8dd7124bdbfe572879ebe484
Edward <ed.f.jones@gmail.com>
2023-04-03 16:12:24 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(length){
   // Write your code here!
+  if (length <= 400) {
+    return "This one is on me!"
+
+  } else if (length > 400 && length < 2000){
+    return "That will be twenty bucks."
+
+  } else if (length > 2000 && length < 2500){
+    return "I will gladly take your thirty bucks."
+
+  } else {
+    return "No can do."
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC"?"Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tips){
   // Write your code here!
+  switch (tips) {
+    case "generous":
+      return "Thank you so much."
+
+   case "not as generous":
+      return "Thank you."
+
+    default:
+      return "Bye."
+  }
 }
